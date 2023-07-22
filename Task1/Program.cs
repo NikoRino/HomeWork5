fc45c658226e040df12b46d3d65c9aec2702f14e
NikoRino <niko89rino@gmail.com>
2023-07-22 15:15:55 +0700

    Внес первую задачу

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GenerationRandomArray(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+int GetEvenElements(int[] array, bool positive)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (positive)
+        {
+            if (array[i] % 2 == 0) count++;
+        }
+    }
+    return count;
+}
+
+int number = GetInput("Введите количество элементов массива: ");
+int[] array = GenerationRandomArray(number);
+Console.WriteLine($"Массив [{String.Join(",", array)}]");
+Console.WriteLine($"Количество четных чисел {GetEvenElements(array, true)}");
